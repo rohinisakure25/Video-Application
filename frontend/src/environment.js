f5b29eea58c5303f3b0a842dcf1d97499e4b1ab4
rohinisakure25 <sakurerohini5@gmail.com>
2026-01-09 12:54:35 +0530

    Connect frontend to live backend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://video-application-qkw2.onrender.com" :
+    "https://video-application-qkw2.onrender.com":
 
     "http://localhost:8000"
 
